8b59d4611cf787d9b3c0e4006e0d7f591ded99ae
sajadevo <nawabisajadahmad@gmail.com>
2024-11-27 15:59:04 +0430

    update the product

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
 import type { Config } from "tailwindcss";
+import { mtConfig } from "@material-tailwind/react";
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [mtConfig],
 } satisfies Config;
